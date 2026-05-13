682d57c183f8cae50df190f3aef0d0d7b67e5ce9
Dan Holliday <dan@integrate-it.co.uk>
2026-05-13 09:11:59 +0100

    Fixed logging issue.

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	stdlog "log"
 	"os"
+	"path/filepath"
+	"time"
 
 	gohttp "net/http"
 
@@ -30,19 +32,88 @@ var exitCh chan int
 var version string
 
 func configureLogging() {
-	logFilePath := os.Getenv("MH_LOG_FILE")
-	if logFilePath == "" {
-		return
+	logFilePath := resolveLogFilePath()
+
+	if err := rotateLogFileIfOlderThan(logFilePath, 24*time.Hour); err != nil {
+		log.Printf("Unable to rotate log file %q: %s", logFilePath, err)
 	}
 
 	file, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Printf("Unable to open MH_LOG_FILE %q: %s", logFilePath, err)
+		log.Printf("Unable to open log file %q: %s", logFilePath, err)
 		return
 	}
 
 	stdlog.SetOutput(io.MultiWriter(os.Stdout, file))
 	log.Printf("Writing logs to %s", logFilePath)
+}
+
+func resolveLogFilePath() string {
+	logFilePath := os.Getenv("MH_LOG_FILE")
+	if logFilePath == "" {
+		logFilePath = "mailhogplus.log"
+	}
+	if filepath.IsAbs(logFilePath) {
+		return logFilePath
+	}
+	absPath, err := filepath.Abs(logFilePath)
+	if err != nil || absPath == "" {
+		return logFilePath
+	}
+	return absPath
+}
+
+func rotateLogFileIfOlderThan(logFilePath string, maxAge time.Duration) error {
+	info, err := os.Stat(logFilePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return err
+	}
+	if time.Since(info.ModTime()) < maxAge {
+		return nil
+	}
+
+	rotatedPath := fmt.Sprintf("%s.%s", logFilePath, info.ModTime().Format("20060102"))
+	if err := os.Remove(rotatedPath); err != nil && !os.IsNotExist(err) {
+		return err
+	}
+	if err := os.Rename(logFilePath, rotatedPath); err != nil {
+		return err
+	}
+
+	entries, err := findOlderRotatedLogFiles(logFilePath, time.Now().Add(-maxAge))
+	if err != nil {
+		return err
+	}
+	for _, path := range entries {
+		if err := os.Remove(path); err != nil && !os.IsNotExist(err) {
+			return err
+		}
+	}
+	return nil
+}
+
+func findOlderRotatedLogFiles(basePath string, cutoff time.Time) ([]string, error) {
+	matches, err := filepath.Glob(basePath + ".*")
+	if err != nil {
+		return nil, err
+	}
+	old := make([]string, 0, len(matches))
+	for _, path := range matches {
+		info, statErr := os.Stat(path)
+		if statErr != nil {
+			if os.IsNotExist(statErr) {
+				continue
+			}
+			return nil, statErr
+		}
+		if info.ModTime().Before(cutoff) {
+			old = append(old, path)
+		}
+	}
+	return old, nil
 }
 
 func configure() {
